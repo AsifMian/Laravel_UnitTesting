e9607d54530debe21fefe9b644f4256cb17880d8
Asif <asif43997@gmail.com>
2022-08-28 11:45:45 +0500

    Basic feature test

---
+++ tests/Feature/ShowRoomsControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ShowRoomsControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/rooms');
+
+        $response->assertStatus(200)
+            // ->assertSeeText('Rooms');
+            ->assertViewIs('rooms.index')
+            ->assertViewHas('rooms');
+    }
+}
